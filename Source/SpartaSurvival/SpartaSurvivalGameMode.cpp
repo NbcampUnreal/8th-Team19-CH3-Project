665ee341027e84b7d783445871852520eeebec08
kangminsu123 <xlfhf123@gmail.com>
2026-05-11 20:33:26 +0900

    캐릭터 메타휴먼 변경 [강민수]

--- Source/SpartaSurvival/SpartaSurvivalGameMode.cpp
+++ Source/SpartaSurvival/SpartaSurvivalGameMode.cpp
@@ -21,7 +21,7 @@ ASpartaSurvivalGameMode::ASpartaSurvivalGameMode()
 
 	EnemySpawnComp = CreateDefaultSubobject<UEnemySpawnComponent>(TEXT("EnemySpawnComp"));
 
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Script/Engine.Blueprint'/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C'"));
+	/*static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Script/Engine.Blueprint'/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C'"));
 
 	if (PlayerPawnBPClass.Class != nullptr)
 	{
@@ -31,7 +31,7 @@ ASpartaSurvivalGameMode::ASpartaSurvivalGameMode()
 	else
 	{
 		UE_LOG(LogTemp, Error, TEXT("실패: 메인 캐릭터 클래스를 찾지 못했습니다. 경로 확인해봐!"));
-	}
+	}*/
 
 	AccumulatedSeconds = 0;
 	CurrentStage = 1;
