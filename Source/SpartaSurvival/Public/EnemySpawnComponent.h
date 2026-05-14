a4488f7c5634afb587719ba0b52584f141aa1d7e
kangminsu123 <xlfhf123@gmail.com>
2026-05-14 13:50:37 +0900

    .

--- Source/SpartaSurvival/Public/EnemySpawnComponent.h
+++ Source/SpartaSurvival/Public/EnemySpawnComponent.h
@@ -3,6 +3,7 @@
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "EnemyWaveData.h"
+#include "EnemyBase.h"
 #include "EnemySpawnComponent.generated.h"
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
@@ -18,7 +19,7 @@ public:
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
     class UDataTable* WaveDataTable;
 
-    
+
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning")
     FEnemyWaveData CurrentWaveInfo;
 
@@ -32,5 +33,7 @@ public:
 
 private:
     FTimerHandle SpawnTimerHandle;
+    TArray<TSubclassOf<AEnemyBase>> WaveSpawnQueue;
+    FVector LastSpawnCenter;
 
 };
